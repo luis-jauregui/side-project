3f6853e2faab0da338ed828da302342cfb015dd1
Luis Enrique <lu.enriquejv@gmail.com>
2023-05-14 16:41:36 -0500

    initial commit

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            dgProducts = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dgProducts).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dgProducts
+            // 
+            dgProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgProducts.Location = new Point(12, 240);
+            dgProducts.Name = "dgProducts";
+            dgProducts.RowTemplate.Height = 25;
+            dgProducts.Size = new Size(776, 198);
+            dgProducts.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Categoria";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(105, 24);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(181, 23);
+            comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(686, 96);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 50);
+            button1.TabIndex = 4;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(dgProducts);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 456);
+            panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 456);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgProducts).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dgProducts;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Panel panel1;
     }
 }
